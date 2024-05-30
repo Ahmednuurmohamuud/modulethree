914417918cb0c773edec8fb71f2cf9a36c334832
Ahmednuurmohamuud <xararavic1547@gmail.com>
2024-05-30 12:34:45 +0300

    new file

---
+++ matrix.py
@@ -0,0 +1,70 @@
+
+
+#matrix= [[12,4,0,9],[10,5,6,1],[0,7,5,9],[3,5,8,0]]
+
+#print matrix
+#for i in range(4):
+    #for j in range(4):
+     #print(matrix[i][j] ,end="\t   ")
+
+    #print()
+    
+
+#print()
+
+#double
+#for i in range(4):
+    #for j in range(4):
+    #print(matrix[i][j] * 2 ,end="\t  ")
+
+    #print()
+
+
+#print()
+
+
+#exchange
+
+#for j in range(4):
+    #for i in range(4):
+    # print(matrix[i][j] ,end="\t   ")
+
+    #print()
+
+matrix1=[[12 , 10 , 0] ,[4  , 5  , 7] ,[0  , 6  , 5]]
+
+
+matrix2=[[9 , 1 , 9 ],[7 , 2 , 3 ] ,[8 , 5 , 0 ]]
+
+print(matrix1)
+print(matrix2)
+
+
+
+
+
+
+for j in range(3):
+    for i in range(3):
+        matrix3=(matrix1[i][j] + matrix2[i][j])
+        print(matrix3,end="\t   ")
+
+print()    
+
+for j in range(3):
+    for i in range(3):
+        matrix3=(matrix1[i][j] - matrix2[i][j] )
+        print(matrix3,end="\t   ")
+
+print() 
+
+
+          
+
+
+    
+
+
+
+
+
