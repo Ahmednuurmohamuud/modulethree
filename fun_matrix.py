86d1fd6cf372e2f9e2a99150f97f50a4cf644ff7
Ahmednuurmohamuud <xararavic1547@gmail.com>
2024-06-05 20:37:26 +0300

    added

---
+++ fun_matrix.py
@@ -0,0 +1,31 @@
+# string
+name = "Jaamac Geedi"
+
+# upper case
+upper = name.upper()
+
+ # change
+change = name. split ()
+change_name = ' '. join (reversed (change) )
+
+
+
+
+
+
+# reverse
+reversed_name = ""
+for char in name:
+    reversed_name = char + reversed_name
+
+
+
+print( name)
+print( upper)
+print(change_name)
+print (reversed_name)
+
+
+
+
+
