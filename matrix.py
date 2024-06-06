eb07ca11cecba0167b880ba726de7eec6dfe785e
Ahmednuurmohamuud <xararavic1547@gmail.com>
2024-06-06 11:54:40 +0300

    added new file

--- matrix.py
+++ matrix.py
@@ -31,32 +31,38 @@
 
     #print()
 
-matrix1=[[12 , 10 , 0] ,[4  , 5  , 7] ,[0  , 6  , 5]]
+
+matrix1 = [[1, 2, 3], [4, 5, 6]]
+matrix2 = [[7,8], [9, 10], [11,12]]
+matrix3 = [[0,0], [0, 0]]
+
+#print(matrix)
+
+def print_matrix(matrix):
+    print(" -\t-")
+    for i in range(len(matrix)):
+        print("|", end=" ")
+        for j in range(len(matrix[0])):
+            print(matrix[i][j], end=" ")
+        print(" |", end="  ")
+        print()
+    print(" -\t-")
+
+def multiply_matrix(mat_1, mat_2):
+    for i in range(len(mat_1)):
+        for j in range(len(mat_2[0])):
+            for k in range(len(mat_2)):
+                matrix3[i][j] += mat_1[i][k] * mat_2[k][i]
+            print_matrix(matrix3)
+
+print_matrix(matrix1)
+print_matrix(matrix2)
+
+multiply_matrix(matrix1, matrix2)
+print_matrix(matrix3)
 
 
-matrix2=[[9 , 1 , 9 ],[7 , 2 , 3 ] ,[8 , 5 , 0 ]]
 
-print(matrix1)
-print(matrix2)
-
-
-
-
-
-
-for j in range(3):
-    for i in range(3):
-        matrix3=(matrix1[i][j] + matrix2[i][j])
-        print(matrix3,end="\t   ")
-
-print()    
-
-for j in range(3):
-    for i in range(3):
-        matrix3=(matrix1[i][j] - matrix2[i][j] )
-        print(matrix3,end="\t   ")
-
-print() 
 
 
           
